afca56f33c44f854fac8f5a38435b34f8478781d
Mai Truong <maitruong2801@gmail.com>
2018-07-24 01:44:58 -0400

    add basic methods

--- analyser.py
+++ analyser.py
@@ -1,87 +1,119 @@
-import re, string
+import re, string, sys
+from itertools import islice
 from collections import Counter
 file = open('a-tale-of-two-cities.txt', 'r')
-# text = file.read().lower()
-short = 'a , very , short . example ; to - test ` punctuation'
-long = '''
-From an efficiency perspective, you're not going to beat
-
-s.translate(None, string.punctuation)
-It's performing raw string operations in C with a lookup table - there's not much that will beat that but writing your own C code.
-
-If speed isn't a worry, another option though is:
-
-exclude = set(string.punctuation)
-s = ''.join(ch for ch in s if ch not in exclude)
-This is faster than s.replace with each char, but won't perform as well as non-pure python approaches such as regexes or string.translate, as you can see from the below timings. For this type of problem, doing it at as low a level as possible pays off.
-
-Timing code:
-
-import re, string, timeit
-
-s = "string. With. Punctuation"
-exclude = set(string.punctuation)
-table = string.maketrans("","")
-regex = re.compile('[%s]' % re.escape(string.punctuation))
-
-def test_set(s):
-    return ''.join(ch for ch in s if ch not in exclude)
-
-def test_re(s):  # From Vinko's solution, with fix.
-    return regex.sub('', s)
-
-def test_trans(s):
-'''
+text = file.read()
 
 def getListOfWords(text):
-    list = text.translate(string.maketrans("", ""), string.punctuation)
-    return re.split('\s+', list)
+    word_list = text.translate(string.maketrans("", ""), string.punctuation)
+    return re.findall('\w+', word_list)
 
-def getTotalNumberOfWords(list):
-    return len(list)
+def getTotalNumberOfWords(word_list):
+    return len(word_list)
 
 ''' returns the number of UNIQUE words in the novel. '''
-def getTotalUniqueWords(list):
+def getTotalUniqueWords(word_list):
     cnt = Counter()
-    for word in list:
+    for word in word_list:
         cnt[word] += 1
     return cnt
 
 ''' return the 20 most frequently used words in the novel and the number of times they were used '''
-def get20MostFrequentWords(list):
-    return list.most_common(20)
+def get20MostFrequentWords(word_list):
+    return Counter(word_list).most_common(20)
+
+''' returns the 20 LEAST frequently used words and the number of times they were used '''
+def get20LeastFrequentWords(word_list):
+    return Counter(word_list).most_common()[:-21:-1]
 
 ''' filters the most common 100 English words and then returns the 20 most
  frequently used words and the number of times they were used '''
-def get20MostInterestingFrequentWords():
+def get20MostInterestingFrequentWords(word_list):
+    file = open('most-common-words.txt')
+    common_word = re.findall('\w+', file.read().lower())
+    common_word = common_word[:100]
+
+    mostInteresting = []
+    mostCommon120 = Counter(word_list).most_common(120)
+    for (word, cnt) in mostCommon120:
+        if word not in common_word:
+            mostInteresting.append((word, cnt))
+
+    return mostInteresting[:20]
+
+''' Get headings and text of chapters
+Got the code idea from https://github.com/JonathanReeve/chapterize/blob/master/chapterize/chapterize.py
+'''
+def getChapters(text):
+    lines = text.split('\n')
+    romanNumerals = '(?=[MDCLXVI])M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})'
+    enumerators = romanNumerals
+    separators = '(\. | )'
+    titleCase = '[A-Z][a-z]'
+
+    pat = re.compile(enumerators + separators + titleCase)
+
+    headings = []
+    headlines = []
+    for i, line in enumerate(lines):
+        if pat.match(line) is not None:
+            # heading = line.split(' ', 1)
+            # headings[heading[0]] = heading[1]
+            headings.append(line)
+            headlines.append(i)
+
+    chapters = []
+    lastHeadline = len(headlines) - 1
+    for i, headline in enumerate(headlines):
+        if i is not lastHeadline:
+            nextHeadline = headlines[i+1]
+            chapters.append(lines[headline+1:nextHeadline])
+
+    return headings, chapters
+
+def getFrequencyOfWord(word, chapters):
+    freq = []
+    for chapter in chapters:
+        cnt = 0
+        for line in chapter:
+            cnt = cnt + line.count(word)
+        freq.append(cnt)
+    return freq
+
+def getChapterQuoteAppears(quote, headings, chapters):
+    for i, chapter in enumerate(chapters):
+        for line in chapter:
+            if quote in line:
+                return i+1
+    return -1
+
+def generateSentence(word_list):
     pass
 
-''' returns the 20 LEAST frequently used words and the number of times they were used '''
-def get20LeastFrequentWords(list):
-    return list[:-21:-1]
-
-def getFrequencyOfWord(text):
-    text.split("Chapter [0-9]+\n")
-
-def getChapterQuoteAppears(list):
+def getAutocompleteSentence(start):
     pass
 
-def generateSentence(list):
-    pass
+word_list = getListOfWords(text)
+num = getTotalNumberOfWords(word_list)
+total = getTotalUniqueWords(word_list)
+mostCommon20 = get20MostFrequentWords(word_list)
+interesting20 = get20MostInterestingFrequentWords(word_list)
+leastCommon20 = get20LeastFrequentWords(word_list)
+headings, chapters = getChapters(text)
+frequency = getFrequencyOfWord('Bastille', chapters)
+chapterQuote = getChapterQuoteAppears('it was the spring of hope', headings, chapters);
 
-list = getListOfWords(short)
-num = getTotalNumberOfWords(list)
-total = getTotalUniqueWords(list)
-mostCommon20 = get20MostFrequentWords(total)
-leastCommon20 = get20LeastFrequentWords(mostCommon20)
-
-print '\nList of words: \n'
-print list
-print '\nTotal number of words: \n'
-print num
-print '\nTotal unique words: \n'
-print total
-print '\n20 Most common words: \n'
-print mostCommon20
-print '\n20 Least common words: \n'
-print leastCommon20
+# print '\nList of words: \n', word_list
+# print '\nTotal number of words: \n', num
+# print '\nTotal unique words: \n', len(total)
+# print '\n20 Most common words: \n'
+# print mostCommon20
+# for i, (word, cnt) in enumerate(mostCommon20):
+#     print word, cnt
+# print '\n20 Least common words: \n', leastCommon20
+# for (word, cnt) in leastCommon20:
+#         print word, cnt
+# print '\n20 Most interesting frequent words\n', interesting20
+# for (word, cnt) in interesting20: print word, cnt
+# print '\nFrequencies of words in chapters: \n', frequency
+# print '\nQuote belongs to chapter', chapterQuote
